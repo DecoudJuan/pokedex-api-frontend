71d970074f0c2653806c672800993f19c1b4c1e4
DecoudJuan <decoudjuanmanuel1@gmail.com>
2025-08-17 23:29:35 -0300

    feat: added images

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,22 +1,26 @@
 type Props = {
-    name: string;
-    imageUrl?: string;
-    type: string;
-  };
-  
-  export default function PokemonCard({ name, imageUrl, type }: Props) {
-    return (
-      <div className="bg-white ring-1 ring-slate-200 rounded-xl p-3 shadow-sm hover:shadow transition">
-        <div className="relative">
-          <div className="aspect-square bg-slate-100 rounded-lg overflow-hidden">
-            {imageUrl ? (
-              <img src={imageUrl} alt={name} className="w-full h-full object-cover" />
-            ) : null}
-          </div>
+  name: string;
+  imageUrl?: string;
+  type: string;
+};
+
+export default function PokemonCard({ name, imageUrl, type }: Props) {
+  const defaultImage =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXXdYR-FXBMWaKl4eu5o4TUuGmaH9HOxK2TQ&s";
+
+  return (
+    <div className="bg-white ring-1 ring-slate-200 rounded-xl p-3 shadow-sm hover:shadow transition">
+      <div className="relative">
+        <div className="aspect-square bg-slate-100 rounded-lg overflow-hidden">
+          <img
+            src={imageUrl || defaultImage}   
+            alt={name}
+            className="w-full h-full object-cover"
+          />
         </div>
-        <div className="mt-2 font-medium capitalize text-center">{name}</div>
-        <div className="text-sm text-slate-500 text-center">{type}</div>
       </div>
-    );
-  }
-  
+      <div className="mt-2 font-medium capitalize text-center">{name}</div>
+      <div className="text-sm text-slate-500 text-center">{type}</div>
+    </div>
+  );
+}
